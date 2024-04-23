7fab104b433b5a2cbfc9b91e9b661657368f0683
Will Luft <william.d.luft@gmail.com>
2024-04-23 16:07:48 -0400

    Updated file name for data arrays/objects

--- src/components/LeatherImageGrid/LeatherImageGrid.js
+++ src/components/LeatherImageGrid/LeatherImageGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LEATHER_PHOTOS } from '../../leatherPhotos';
+import { LEATHER_PHOTOS } from '../../data';
 
 function LeatherImageGrid() {
   return (
