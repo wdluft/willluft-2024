0d0614b6e557c416ea7336f7d247bb591a0a2e63
Will Luft <william.d.luft@gmail.com>
2024-04-24 10:57:08 -0400

    Added main tag and styling to error page

--- src/routes/error-page.js
+++ src/routes/error-page.js
@@ -1,5 +1,6 @@
 import { useRouteError } from 'react-router-dom';
 import Header from '../components/Header';
+import styled from 'styled-components';
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -8,13 +9,20 @@ export default function ErrorPage() {
   return (
     <>
       <Header />
-      <div id='error-page'>
+      <MainWrapper id='error-page'>
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
-      </div>
+      </MainWrapper>
     </>
   );
 }
+
+const MainWrapper = styled.main`
+  padding-left: var(--padding-left);
+  padding-right: var(--padding-left);
+  padding-bottom: var(--size-3);
+  max-width: var(--max-content-width);
+`;
